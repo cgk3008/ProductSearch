aaef25e7c3d7fce94bb2e43c667044364cc02f86
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-07-31 18:12:12 -0400

    Add project files.

---
+++ MVVM Product search/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVVM_Product_search
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVVM Product search/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVVM_Product_search.MvcApplication" Language="C#" %>
